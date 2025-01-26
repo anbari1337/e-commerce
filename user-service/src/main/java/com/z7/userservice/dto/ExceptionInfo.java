64d9042cc61a0532137bc32a7a3bbd720657c958
anbari1337 <amine.a@wiggli.io>
2025-01-26 11:48:50 +0100

    fix: add @Getter to ExceptionInfo class

--- user-service/src/main/java/com/z7/userservice/dto/ExceptionInfo.java
+++ user-service/src/main/java/com/z7/userservice/dto/ExceptionInfo.java
@@ -1,9 +1,12 @@
 package com.z7.userservice.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+
 
 import java.time.LocalDateTime;
 
+@Getter
 @Builder
 public class ExceptionInfo {
     private String url;

eb7759933a8f44bae08c347e19ee3cd417137b8e
anbari1337 <amine.a@wiggli.io>
2025-02-28 16:01:16 +0000

    feat(kafka): product kafka producer

---
+++ product-service/src/main/java/com/aanbari/productservice/config/KafkaProductProducer.java
@@ -0,0 +1,47 @@
+package com.aanbari.productservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProductProducer {
+
+
+    @Value("${kafka.bootstrap.server}")
+    private String bootstrapServer;
+
+    private Map<String, Object> producerConfig(){
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public <T> ProducerFactory<String, T> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public <T> KafkaTemplate<String, T> kafkaTemplate(ProducerFactory<String, T> pf){
+        return new KafkaTemplate<>(pf);
+    }
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("new-product-topic").build();
+    }
+}

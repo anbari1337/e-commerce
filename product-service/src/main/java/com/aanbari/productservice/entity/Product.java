09da51eeaf759d976d3ff429a8f7a9a8eeae38c4
anbari1337 <amine.a@wiggli.io>
2025-03-03 22:43:05 +0000

    feat: import data sql file

--- product-service/src/main/java/com/aanbari/productservice/entity/Product.java
+++ product-service/src/main/java/com/aanbari/productservice/entity/Product.java
@@ -22,7 +22,7 @@ import java.time.Instant;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
     private String name;
